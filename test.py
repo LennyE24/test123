9e44e7464dfc8cae2e290f0d9c046928861c7150
Lennart Engel <Lennart.Engel@winamax.local>
2019-09-17 15:00:48 +0200

    hgjghj

--- test.py
+++ test.py
@@ -11,3 +11,5 @@ print("new text written in repo on Github!!!")
 print("Laaaaaaaaaaaaaaaaast test!!!")
 
 print("Ok now I try to make a change in VS code, push it on github and then open it with Atom!!!")
+
+print("Same thing from different side!!!")
