0ed7c30de98e6ac901443ff957ce54418a77ef0d
Lennart Engel <Lennart.Engel@winamax.local>
2019-09-17 14:59:10 +0200

    VS Code/Atom

--- test.py
+++ test.py
@@ -9,3 +9,5 @@ print("new text written in repo on Github!!!")
 
 
 print("Laaaaaaaaaaaaaaaaast test!!!")
+
+print("Ok now I try to make a change in VS code, push it on github and then open it with Atom!!!")
